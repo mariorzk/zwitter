f03f51163b5c7f3482ea2bddcae4a2c1d6da570c
MarioRZK <mariorzk@gmail.com>
2024-09-17 11:59:01 -0300

    criação do form de login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,4 @@
+import { SigninForm } from "@/components/auth/signin-form"
 import { Logo } from "@/components/ui/logo"
 import Link from "next/link"
 
@@ -7,7 +8,7 @@ const Page = () => {
             <Logo size={56} />
             <h1 className="mt-10 text-2xl">Entre na sua conta</h1>
             <div className="mt-10 mb-14 flex flex-col gap-6">
-                Formulário
+                <SigninForm />
             </div>
             <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
                 <div className="text-gray-500">Ainda não tem uma conta?</div>
