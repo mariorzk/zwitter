0de897e7a87f0a4904844ca3ffc28499efe0ee7e
MarioRZK <mariorzk@gmail.com>
2024-09-17 11:42:48 -0300

    estrutura da página de login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,7 +1,18 @@
+import { Logo } from "@/components/ui/logo"
+import Link from "next/link"
+
 const Page = () => {
     return (
         <div className="max-w-lg mx-auto mt-12 px-6">
-            LOGIN
+            <Logo size={56} />
+            <h1 className="mt-10 text-2xl">Entre na sua conta</h1>
+            <div className="mt-10 mb-14 flex flex-col gap-6">
+                Formulário
+            </div>
+            <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
+                <div className="text-gray-500">Ainda não tem uma conta?</div>
+                <Link href="/signup" className="hover:underline">Cadastre-se</Link>
+            </div>
         </div>
     )
 }
